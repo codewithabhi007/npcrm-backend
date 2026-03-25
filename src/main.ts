62ca74770e4100bedcf1902a1714bc5ade805f17
codewithabhi007 <abhishek.bharti@conceptsparx.com>
2026-03-25 14:07:41 +0530

    initial

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
