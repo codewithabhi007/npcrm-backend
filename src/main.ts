0e8ec92a4f841d21e0fd5423b0d85fbcb86ce33f
codewithabhi007 <abhishek.bharti@conceptsparx.com>
2026-03-25 16:21:49 +0530

    testing auto deployemnt

--- src/main.ts
+++ src/main.ts
@@ -15,5 +15,6 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(3000, '0.0.0.0');
+  console.log('Server is running on port 3000');
 }
 bootstrap();
